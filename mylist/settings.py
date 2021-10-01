6960211246cd4de79c4b7136996ccfefc1aba60b
Bhubanghimire <bhubanghimire3130@gmail.com>
2021-10-01 15:02:19 +0545

    done

--- mylist/settings.py
+++ mylist/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'mylist.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'bhuban',
+       'USER': 'postgres',
+       'PASSWORD': '8924',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
