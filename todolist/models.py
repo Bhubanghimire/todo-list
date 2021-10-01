ffce21ca25906893aab32278fae5949f187bb0d3
Bhubanghimire <bhubanghimire3130@gmail.com>
2021-10-01 16:01:50 +0545

    Adafsd

--- todolist/models.py
+++ todolist/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Todolist(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     text = models.CharField(max_length=500)
     completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
